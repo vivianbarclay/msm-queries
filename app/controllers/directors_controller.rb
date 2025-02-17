5ca4e4147d194b7efb24675ae2819a1955eb43df
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-17 21:50:52 +0000

    final commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,11 +1,19 @@
 class DirectorsController < ApplicationController
   def index
-    @list_of_directors = Director.all.order({ dob: :desc, name: :asc }) # Sort by DOB descending
+    @list_of_directors = Director.all.order({ dob: :desc, name: :asc })
   end
 
   def show
     @director = Director.find(params[:id]) 
-    @movies = Movie.where(director_id: @director.id).order(year: :desc) # Fetch movies for the director
+    @movies = Movie.where(director_id: @director.id).order(year: :desc)
   end
   
+  def youngest
+    @youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+  end
+
+  def eldest
+    @eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { controller: "directors", action: "index" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/directors/:id", { controller: "directors", action: "show" }) 
   get("/actors", { controller: "actors", action: "index" })
   get("/actors/:id", { controller: "actors", action: "show" })
   get("/movies", { controller: "movies", action: "index" })
   get("/movies/:id", { controller: "movies", action: "show" })
-end
+  end
